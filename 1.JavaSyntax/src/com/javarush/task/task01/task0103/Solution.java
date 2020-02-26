3636ce4e19e361d1e992c8dc2d02427b3a7af39c
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-02-26 18:09:38 +0300

    task4_level0

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task01.task0103;
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3126;
+        int b = 8;
+        int c = a-b;
+        System.out.println(c);
+    }
+}
