d96e3b47ab4910442c25d50c5b2e7efcd2d35796
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-02-26 18:07:24 +0300

    task3_level0

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0102;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        
+    }
+}
