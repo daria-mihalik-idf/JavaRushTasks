a49440439ee447fa14641f665ac21f7d1530429d
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-02-26 18:27:55 +0300

    level0_lecture8

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0106/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task01.task0106;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Это не баг, это фича.");
+
+    }
+
+}
