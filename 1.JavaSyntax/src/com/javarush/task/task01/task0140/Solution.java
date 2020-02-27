3f40a098b7e4a6bd06a52384c21031aff5128393
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-02-27 12:29:50 +0300

    leve1_lecture3_4

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task01.task0140;
+
+
+public class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        int a = number*number;
+        System.out.println(a);
+    }
+}
