188edb343843095a3a6a4e0c511ed22cca0224e5
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-04-16 08:45:41 +0300

    Core_level11_15

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1528/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task15.task1528;
+
+/* 
+ООП. Hrivna — тоже деньги
+Исправь класс Hrivna так, чтоб избежать возникновения ошибки StackOverflowError.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Hrivna().getAmount());
+    }
+
+    public static abstract class Money {
+        abstract Money getMoney();
+
+        public Object getAmount() {
+            return getMoney().getAmount();
+        }
+    }
+
+    //add your code below - добавь код ниже
+
+    public static class Hrivna extends Money {
+        public double amount = 123d;
+
+        public Hrivna getMoney() {
+            return this;
+        }
+
+        @Override
+        public Object getAmount() {
+            return this.amount;
+        }
+    }
+}
