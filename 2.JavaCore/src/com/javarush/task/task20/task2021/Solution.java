20cc96b329903beb695c26615054f14b60186acf
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-04-29 10:21:33 +0300

    Core_level_11_20

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task20.task2021;
+
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/*
+Сериализация под запретом
+Запрети сериализацию класса SubSolution используя NotSerializableException.
+Сигнатуры классов менять нельзя.
+
+
+Требования:
+1. Класс Solution должен поддерживать интерфейс Serializable.
+2. Класс SubSolution должен быть создан внутри класса Solution.
+3. Класс SubSolution должен быть потомком класса Solution.
+4. При попытке сериализовать объект типа SubSolution должно возникать исключение NotSerializableException.
+5. При попытке десериализовать объект типа SubSolution должно возникать исключение NotSerializableException.*/
+
+
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution {
+        public void writeObject(ObjectOutputStream out) throws NotSerializableException {
+            throw new NotSerializableException();
+        }
+
+        public void readObject(ObjectInputStream in) throws NotSerializableException {
+            throw new NotSerializableException();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
